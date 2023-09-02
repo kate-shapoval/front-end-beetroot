75f28c2e213cae551849f7706e9b62bb726c485b
KateOvsienko <kate.shapoval@gmail.com>
2023-09-02 08:19:13 +0300

    fix pr

--- React-lessons/React-project/front/src/App.js
+++ React-lessons/React-project/front/src/App.js
@@ -1,10 +1,5 @@
-//import logo from './logo.svg';
-// import './App.css';
-
-
 import { Route, Routes } from "react-router-dom";
 
-// import Home from './Pages/Home'
 import Home from './Pages/Home.jsx'
 import Prices from './Pages/Prices.jsx'
 import Portfolio from './Pages/Portfolio.jsx'
@@ -13,13 +8,8 @@ import ContactUs from './Pages/ContactUs.jsx'
 
 import Header from './components/common/Header/Header.jsx'
 import Footer from './components/common/Footer/Footer.jsx'
-import Hero from "./components/Hero/Hero.jsx";
-
 
 import './assets/sass/main.scss'
-
-import { shopItems } from './data/shopItems.js'
-let currentItems = [...shopItems];
 
 function App() {
   return (
@@ -37,24 +27,6 @@ function App() {
       <Footer />
     </div>
   )
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
